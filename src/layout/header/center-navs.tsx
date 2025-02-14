5dc1eebb85214064fa335aef19da4e01a14a7450
sayoriqwq <2531600563@qq.com>
2025-02-14 20:34:58 +0800

    fix: center-navs state bug

--- src/layout/header/center-navs.tsx
+++ src/layout/header/center-navs.tsx
@@ -1,9 +1,13 @@
+'use client'
+
 import type { HeaderTabs } from '@/atoms/header'
 import { headerSelectedAtom } from '@/atoms/header'
 import { cn } from '@/lib/utils'
 import { useAtom } from 'jotai'
 import { BookText, Link2, LoaderCircle, MessageCircle } from 'lucide-react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { useEffect } from 'react'
 
 const navs = [
   {
@@ -30,32 +34,39 @@ const navs = [
 
 export function CenterNavs() {
   const [selected, setSelected] = useAtom(headerSelectedAtom)
+  const pathname = usePathname()
+
+  useEffect(() => {
+    const matchedNav = navs.find(nav => pathname.startsWith(nav.href))
+    if (matchedNav) {
+      setSelected(matchedNav.href as HeaderTabs)
+    }
+  }, [pathname, setSelected])
+
   return (
     <div className="flex">
-      {
-        navs.map(nav => (
-          <Link
-            href={nav.href}
+      {navs.map(nav => (
+        <Link
+          href={nav.href}
+          className={cn(
+            'relative rounded-md px-2 py-1 flex-center gap-2',
+            'hover:text-yume-spotlight-foreground',
+            'transition-all duration-300 ease-in-out',
+            selected === nav.href && 'text-yume-spotlight-foreground px-4',
+          )}
+          key={nav.href}
+        >
+          <div
             className={cn(
-              'relative rounded-md px-2 py-1 flex-center gap-2',
-              'hover:text-yume-spotlight-foreground',
-              'transition-all duration-300 ease-in-out',
-              selected === nav.href && 'text-yume-spotlight-foreground px-4',
-            )}
-            key={nav.href}
-            onClick={() => setSelected(nav.href as HeaderTabs)}
-          >
-            <div className={cn(
               'w-0 scale-0 transition-all duration-300 ease-in-out opacity-0',
               selected === nav.href && 'w-4 scale-100 opacity-100',
             )}
-            >
-              <nav.icon className="size-4" />
-            </div>
-            <span className="relative z-10">{nav.name}</span>
-          </Link>
-        ))
-      }
+          >
+            <nav.icon className="size-4" />
+          </div>
+          <span className="relative z-10">{nav.name}</span>
+        </Link>
+      ))}
     </div>
   )
 }
